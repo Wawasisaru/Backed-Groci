1104d5501a0dd1c7b02a365cc13dbbc93a461c95
Wawasisaru <nsaruwawasi@gmail.com>
2023-05-20 20:09:38 +0300

    made chages in class order

--- order.py
+++ order.py
@@ -1,10 +1,11 @@
 class Order:
     # Attributes
-    def __init__(self, order_id, customer_name,discount_percentage):
+    def __init__(self, order_id, customer_name,discount_percentage,total_amount):
         self.order_id = order_id
         self.customer_name = customer_name
-        self.items = []
+        self.total_amount = total_amount
         self.discount_percentage = discount_percentage
+        self.items = []
     
 
     def calculate_total(self):
@@ -12,11 +13,13 @@ class Order:
         for item in self.items:
             total += item
         return total
+    
     def remove_item(self, item):
-        if item in self.items:
-            self.items.remove(item)
+        if item in self.item:
+            self.item.remove(item)
         else:
             print("Item not found in order.")
 # this method calculates the discount of the total amout
-    def calculate_discounted_amount(self, discount_percentage):
+    def calculate_discounted_amount(self, discount_percentage,total_amount):
         return f"{self.total_amount - (self.total_amount * discount_percentage / 100)}"
+# order = Order(order_id=24, customer_name="Nelly", discount_percentage=5)
